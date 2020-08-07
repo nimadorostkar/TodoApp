e9ae3b2bda1617e14b671f4cfe636f40decc46b6
nima <54316106+nimadorostkar@users.noreply.github.com>
2020-08-07 18:12:42 +0430

    Add files via upload

---
+++ models/todo.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+let todoSchema=new Schema({
+  description:{
+    type:String,
+    required:true,
+  },
+  done:{
+    type:Boolean,
+    default:false
+  },
+})
+module.exports=mongoose.model('Todo',todoSchema);
